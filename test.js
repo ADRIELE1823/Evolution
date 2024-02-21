709590d5bb045f2d14500f06a390de134491e10a
Douglas <douglasevertonev@gmail.com>
2024-02-20 21:48:32 -0300

    deletar tudo

--- test.js
+++
@@ -1,5 +0,0 @@
-<script>
-  document.getElementById("circle").addEventListener("click", function() {
-    window.location.href = "blog.html"// Substitua com a URL desejada
-  });
-</script>
--- welcome.js
+++
@@ -1,7 +0,0 @@
-const loading = document.getElementById('loading');
-const content = document.getElementById('content');
-
-setTimeout(function() {
-  loading.style.display = 'none';
-  content.style.display = 'block';
-}, 5000); 
